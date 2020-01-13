cb08e16fa45577ee5c6c2f27ac8a389635a60367
MacBook <macbook@Anvar.local>
2020-01-13 17:40:21 +0300

    Вывод xml каталога

---
+++ xml2.php
@@ -0,0 +1,32 @@
+<?php 
+include 'bt/pdo.php';
+
+$query = sequery("SELECT * FROM catalog WHERE status != 0");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>XML-catalog</title>
+</head>
+<body>
+	
+	<xml version='1.0' encoding='utf-8'>
+	<root>
+	<?php foreach ($query as $v) { ?>
+	<item>
+		<name>REFAN <?php echo $v['refan']; ?></name><br>
+		<link>https://refanparfum.lv/tovar.php?product-id=<?php echo $v['id']; ?></link><br>
+		<price><?php echo $v['price']; ?></price><br>
+		<image>https://refanparfum.lv/catalog/<?php echo $v['img_url']; ?></image><br>
+		<manufacturer><?php echo getBrand($v['brand']); ?></manufacturer><br>
+		<category>Skaistums un veselība</category><br>
+		<category_full>Skaistums un veselība > <?php if($v['gender']=='0'){echo "Sieviešu smaržas";}else{echo "Vīriešu smaržas";} ?></category_full><br>
+	</item>
+	<br>
+	<br>
+	<?php } ?>
+	</root>
+	</xml>
+</body>
+</html>
