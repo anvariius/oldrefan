1536128bff604b242a750e9f16fcb266f2bc4bd1
MacBook <macbook@Anvar.local>
2020-01-19 16:34:47 +0300

    корзина правка

--- engine.php
+++ engine.php
@@ -2,7 +2,6 @@
 include 'bt/pdo.php';
 ini_set('session.gc_maxlifetime', 950400);
 ini_set('session.cookie_lifetime', 950400);
-session_set_cookie_params(0);
 session_start();
 if (isset($_POST['action'])) {
 	switch ($_POST['action']) {
