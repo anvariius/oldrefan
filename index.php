f28c305a8cc58bbf5d97a5434227ed32ba59eaf4
anvariius <anvar8ku@gmail.com>
2020-04-10 16:36:03 +0300

    Отображение на новинок косметики на главной

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 include 'header.php';
-$query1 = sequery("SELECT * FROM catalog WHERE status != 0 AND intensive !=4 AND novinka = 1 ORDER BY id DESC LIMIT 4");
+$query1 = sequery("SELECT * FROM catalog WHERE status != 0 AND novinka = 1 ORDER BY id DESC LIMIT 4");
 $query3 = sequery("SELECT * FROM catalog WHERE intensive = 1 AND status != 0 ORDER BY id DESC LIMIT 4");
 $popular = sequery("SELECT product_id, COUNT(product_id) FROM actions WHERE action = 0 AND status != 0 GROUP BY product_id ORDER BY COUNT(product_id) DESC LIMIT 4");
 $hits = sequery("SELECT product_id, COUNT(product_id) FROM actions WHERE action = 1 AND status != 0 GROUP BY product_id ORDER BY COUNT(product_id) DESC LIMIT 4");
@@ -39,7 +39,9 @@ $brands_count = $brands_count['COUNT(1)'];
 							<div class="col-md-3 col-sm-6 product-card" product_id="<?php echo $v['id']; ?>" refan="<?php echo $v['refan']; ?>" category="<?php echo $v['gender']; ?>">
 								<div class="product-img" style="background-image: url(catalog/<?php echo $v['img_url']; ?>);">
 									<div class="top-block">
+										<?php if ($v['intensive']!='4'){ ?>
 										<img src="img/intensive.png" class="intensive" alt="" data-toggle="tooltip" data-placement="bottom" data-original-title="Парфюмерная вода. Очень высокая стойкость аромата. Приятный шлейф. Более высокая концетрация ароматических масел в отличии от EDT">
+										<?php } ?>
 										<?php if ($v['status'] == 2) { ?>
 											<span class="rasprodano">Распродано</span>
 										<?php } ?>
@@ -50,9 +52,14 @@ $brands_count = $brands_count['COUNT(1)'];
 									</div>
 									<?php } ?>
 								</div>
+								<?php if ($v['intensive']!='4'){ ?>
 								<h4 class="product-name">REFAN <span><?php echo $v['refan']; ?></span></h4>
 								<h5 class="product-gender"><?php echo getGender($v['gender']); ?> <?php if($v['volume'] !='') echo $v['volume']."ML"; ?></h5>
-								<p class="product-descr">www.parfumanalog.ru</p>
+								<p class="product-descr" style="height: 37px;">www.parfumanalog.ru</p>
+								<?php }else{ ?>
+								<h4 class="product-name1"><?php echo $v['name']; ?></h4>
+								<h5 class="product-gender1"><?php echo $v['serie']; ?></h5>
+								<?php } ?>
 								<p class="product-price">€<?php echo $v['price']; ?></p>
 								<button class="product-opisanie" data-toggle="tooltip" data-placement="top" data-original-title='<?php echo str_replace("<br />", "", $v['descr']); ?>'>Описание товара</button>
 								<button class="add-product">ДОБАВИТЬ В КОРЗИНУ</button>
